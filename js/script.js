3c7e907c62d2e0f05cf02f2e61a4a8ef2746829b
Jona535h <jona535h@edu.easj.dk>
2018-05-28 15:06:06 +0200

    Filterkunktion eksperimenteret med

--- js/script.js
+++ js/script.js
@@ -12,4 +12,37 @@ $(document).ready (function () {
     });
 
 
+
+    $(function(){
+        $('#all').click(function(){
+            $('.aktivitetElement').show();
+            return false;
+        });
+
+        $('#15km').click(function(){
+            $('.aktivitetElement').show();
+            $('.aktivitetElement').not('.a15km').hide();
+            return false;
+        });
+
+        $('#30km').click(function(){
+            $('.aktivitetElement').show();
+            $('.aktivitetElement').not('.a30km').hide();
+            return false;
+        });
+
+        $('#60km').click(function(){
+            $('.aktivitetElement').show();
+            $('.aktivitetElement').not('.a60km').hide();
+            return false;
+        });
+
+        $('#60kmPlus').click(function(){
+            $('.aktivitetElement').show();
+            $('.aktivitetElement').not('.a60kmPlus').hide();
+            return false;
+        });
+    });
+
+
 });
