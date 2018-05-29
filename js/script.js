fcb1dfffc4e813a47cff75e1751dd40b4d146a64
Jona535h <jona535h@edu.easj.dk>
2018-05-29 14:19:26 +0200

    Header tilføjet på alle sider

--- js/script.js
+++ js/script.js
@@ -33,24 +33,21 @@ $(document).ready (function () {
 
         //SKJULER ALLE PÅ NÆR 0-15KM
         $('#15km').click(function(){
-            $('.aktivitetElement').show();
             $('.aktivitetElement').not('.15km').hide();
         });
 
         //SKJULER ALLE PÅ NÆR 15-30KM
         $('#30km').click(function(){
-            $('.aktivitetElement').show();
             $('.aktivitetElement').not('.30km').hide();
         });
 
         //SKJULER ALLE PÅ NÆR 30-60KM
         $('#60km').click(function(){
-            $('.aktivitetElement').show();
             $('.aktivitetElement').not('.60km').hide();
         });
 
+        //SKJULER ALLE PÅ NÆR 60+ KM
         $('#60kmPlus').click(function(){
-            $('.aktivitetElement').show();
             $('.aktivitetElement').not('.60kmPlus').hide();
         });
     });
